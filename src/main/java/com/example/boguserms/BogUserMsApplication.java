29b9a47e4df206f1dd5f3695c1016392106266bd
Daniil Kovalyov <kovalyovd21@gmail.com>
2021-11-24 19:13:29 +0300

    Initial commit

---
+++ src/main/java/com/example/boguserms/BogUserMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.boguserms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogUserMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BogUserMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boguserms/BogUserMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boguserms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogUserMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
